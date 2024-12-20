267ce721d2bdd0e9920ceeb3f868997ee26db905
Arman <arman.kumaraswamy@gmail.com>
2024-12-20 01:36:24 -0500

    add functionality for `touch`, `rm`, `reset` in cli

--- src/components/cli.tsx
+++ src/components/cli.tsx
@@ -1,6 +1,23 @@
 "use client";
 import React, { useEffect, useState, useRef } from "react";
 
+const INITIAL_FILES = [
+  {
+    name: "experience.txt",
+    content:
+      "apten (s24):\n - software engineer intern (may 2024 - july 2024)\n - Next.js, LangChain, AWS CDK\n\nstudydojo (f24):\n - software engineer (october 2023 - march 2024)\n - Next.js, PostgreSQL, NoSQL\n\nsolace health:\n - software engineer intern (july 2023 - october 2023)\n - Next.js, NestJS, PostgreSQL, Redis",
+  },
+  {
+    name: "socials.txt",
+    content:
+      "twitter: ksw_arman\ngithub: armans-code\nlinkedin: armankumaraswamy",
+  },
+  {
+    name: "gui.app",
+    content: "run './gui.app' to open the GUI version of this website",
+  },
+];
+
 const commands = [
   "",
   "whoami",
@@ -9,6 +26,8 @@ const commands = [
   "cat",
   "clear",
   "touch",
+  "rm",
+  "reset",
   "./gui.app",
 ];
 
@@ -25,30 +44,19 @@ interface File {
 function CLI() {
   const [messages, setMessages] = useState<Message[]>([]);
   const [input, setInput] = useState("");
-  const [files, setFiles] = useState<File[]>([
-    {
-      name: "experience.txt",
-      content:
-        "apten (s24):\n - software engineer intern (may 2024 - july 2024)\n - Next.js, LangChain, AWS CDK\n\nstudydojo (f24):\n - software engineer (october 2023 - march 2024)\n - Next.js, PostgreSQL, NoSQL\n\nsolace health:\n - software engineer intern (july 2023 - october 2023)\n - Next.js, NestJS, PostgreSQL, Redis",
-    },
-    {
-      name: "socials.txt",
-      content:
-        "twitter: ksw_arman\ngithub: armans-code\nlinkedin: armankumaraswamy",
-    },
-    {
-      name: "gui.app",
-      content: "run './gui.app' to open the GUI version of this website",
-    },
-  ]);
+  const [files, setFiles] = useState<File[]>(INITIAL_FILES);
 
   const bottomRef = useRef<HTMLDivElement>(null);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     setMessages((prev) => [...prev, { message: input, type: "input" }]);
     if (commands.includes(input.split(" ")[0])) {
       switch (input) {
+        case "reset":
+          setMessages([]);
+          setFiles(INITIAL_FILES);
+          break;
         case "whoami":
           setMessages((prev) => [
             ...prev,
@@ -88,6 +96,16 @@ function CLI() {
         default:
           if (input.startsWith("cat")) {
             const fileName = input.split(" ")[1];
+            if (!fileName) {
+              setMessages((prev) => [
+                ...prev,
+                {
+                  message: "usage: cat <filename> (e.g. cat file.txt)",
+                  type: "output",
+                },
+              ]);
+              break;
+            }
             const file = files.find((file) => file.name === fileName);
             if (file) {
               setMessages((prev) => [
@@ -99,6 +117,81 @@ function CLI() {
                 ...prev,
                 {
                   message: `cat: ${fileName}: No such file or directory`,
+                  type: "output",
+                },
+              ]);
+            }
+          } else if (input.startsWith("touch")) {
+            const fileName = input.split(" ")[1];
+            if (!fileName) {
+              setMessages((prev) => [
+                ...prev,
+                {
+                  message: "usage: touch <filename> (e.g. touch file.txt)",
+                  type: "output",
+                },
+              ]);
+              break;
+            }
+            if (files.find((file) => file.name === fileName)) {
+              setMessages((prev) => [
+                ...prev,
+                {
+                  message: `touch: cannot touch '${fileName}': File exists`,
+                  type: "output",
+                },
+              ]);
+            } else {
+              setInput("");
+              try {
+                const image = await fetch(
+                  `https://cataas.com/cat?width=200&height=200`
+                ).then((data) => data.blob());
+                const imageUrl = URL.createObjectURL(image);
+                // @ts-expect-error we are setting content to an image, not string
+                setFiles((prev) => [
+                  ...prev,
+                  {
+                    name: fileName,
+                    content: (
+                      <img
+                        src={imageUrl}
+                        alt="cat"
+                        className="w-40 h-40 object-cover"
+                      />
+                    ),
+                  },
+                ]);
+              } catch (error) {
+                // in case we get rate limited
+                setFiles((prev) => [
+                  ...prev,
+                  {
+                    name: fileName,
+                    content: "🐈",
+                  },
+                ]);
+              }
+            }
+          } else if (input.startsWith("rm")) {
+            const fileName = input.split(" ")[1];
+            if (!fileName) {
+              setMessages((prev) => [
+                ...prev,
+                {
+                  message: "usage: rm <filename> (e.g. rm file.txt)",
+                  type: "output",
+                },
+              ]);
+              break;
+            }
+            if (files.find((file) => file.name === fileName)) {
+              setFiles((prev) => prev.filter((file) => file.name !== fileName));
+            } else {
+              setMessages((prev) => [
+                ...prev,
+                {
+                  message: `rm: ${fileName}: No such file or directory`,
                   type: "output",
                 },
               ]);
