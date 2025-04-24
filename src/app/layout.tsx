c04bfbbcff734a43fdb400a7fcf504816f4f53ce
Arman <arman.kumaraswamy@gmail.com>
2025-04-24 03:50:55 -0400

    update favicon and keywords

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,15 @@ const geistMono = localFont({
 export const metadata: Metadata = {
   title: "arman's living room",
   description: "arman's living room",
-  keywords: ["arman", "kumaraswamy", "arman kumaraswamy", "arman's living room", "armank", "armank.dev", "armank dev"],
+  keywords: [
+    "arman",
+    "kumaraswamy",
+    "arman kumaraswamy",
+    "arman's living room",
+    "armank",
+    "armank.dev",
+    "armank dev",
+  ],
 };
 
 export default function RootLayout({
